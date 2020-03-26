d27d4e6b98b14368a65ed48f87925e830937e725
Ryan McKenna <ryanpatmckenna@gmail.com>
2020-03-26 16:15:28 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `5kqpqnkvzk2q`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
