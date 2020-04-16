2bdc7e170822a39b07f838e275e425845c1aeec4
Ryan McKenna <ryanpatmckenna@gmail.com>
2020-04-16 04:13:19 -0400

    Update DonateForm.js

--- src/components/Donation/DonateForm.js
+++ src/components/Donation/DonateForm.js
@@ -14,6 +14,7 @@ import Button from "../button"
 import StripeForm from "./StripeForm"
 import AmountSelection from "./AmountSelection"
 
+console.log(process.env.STRIPE_PUBLISHABLE_KEY)
 const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
 
 const DonateContainer = styled.div`
