d3cd06d292bdb4f9e1a1e4c933f264d4aca658ee
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-04-26 14:23:53 -0500

    Adds header with home variant

---
+++ stories/5-Header.stories.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { withKnobs, select, text } from "@storybook/addon-knobs"
+import HomeHeader from "../src/components/Header/HomeHeader"
+import Header from "../src/components/Header/Header"
+
+export default {
+  title: "HomeHeader",
+  component: HomeHeader,
+  decorators: [withKnobs],
+}
+
+const routes = [
+  { title: "About", slug: "about" },
+  { title: "News", slug: "new" },
+  { title: "Projects", slug: "projects" },
+  { title: "Support", slug: "support" },
+]
+
+export const Home = () => (
+  <HomeHeader
+    routes={routes}
+    color={select("Color", ["light", "dark"], "dark")}
+  />
+)
+
+export const Default = () => (
+  <Header color={select("Color", ["light", "dark"], "dark")}>
+    <h1>{text("Header Text", "Masks")}</h1>
+  </Header>
+)
