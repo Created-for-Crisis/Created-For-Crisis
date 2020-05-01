f0316efc1ac96bebb6f8811f96ad0275e996235b
Ryan McKenna <ryanpatmckenna@gmail.com>
2020-05-01 16:57:35 -0400

    Read Description
    - Related Pages Component based off Contentful Data
    - Removed Header & Nav Stories and Replaces with Splash Story
    - Refactored Splash to not have children property
    - Removed all unused JS & Assets. This was long over due since starting the redesign.

--- stories/0-Welcome.stories.js
+++ stories/0-Welcome.stories.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { linkTo } from "@storybook/addon-links"
 import { Container } from "../src/components/Container"
-import { Article } from "../src/components/Article"
 
 export default {
   title: "Welcome",
@@ -9,22 +8,20 @@ export default {
 
 export const Introduction = () => (
   <Container size="article" padded>
-    <Article>
-      <h1>Created for Crisis Storybook</h1>
-      <p>
-        This is the storybook collection for the Created for Crisis Design
-        System. The designs are viewable in Figma <a>here</a>.
-      </p>
-      <h2>Components</h2>
-      <ul>
-        <li onClick={linkTo("Button")}>Button</li>
-        <li onClick={linkTo("Container")}>Container</li>
-      </ul>
-      <h2>Layout Pieces</h2>
-      <ul>
-        <li onClick={linkTo("Button")}>Button</li>
-        <li onClick={linkTo("Container")}>Container</li>
-      </ul>
-    </Article>
+    <h1>Created for Crisis Storybook</h1>
+    <p>
+      This is the storybook collection for the Created for Crisis Design System.
+      The designs are viewable in Figma <a>here</a>.
+    </p>
+    <h2>Components</h2>
+    <ul>
+      <li onClick={linkTo("Button")}>Button</li>
+      <li onClick={linkTo("Container")}>Container</li>
+    </ul>
+    <h2>Layout Pieces</h2>
+    <ul>
+      <li onClick={linkTo("Button")}>Button</li>
+      <li onClick={linkTo("Container")}>Container</li>
+    </ul>
   </Container>
 )
